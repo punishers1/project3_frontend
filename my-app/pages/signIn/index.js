d5edfea66d547a2daba9ef30be81a348c931bfef
KK-kk056 <Khalid.Alyami.eam.js@tuwaiq.edu.sa>
2021-12-20 17:17:06 +0300

    signIn

--- my-app/pages/signIn/index.js
+++ my-app/pages/signIn/index.js
@@ -1,28 +1,44 @@
 import Layout from "../../components/Mylayout"
 import { Form, Button } from 'react-bootstrap';
+import styles from "../../styles/signIn.module.css"
 export default function signIn() {
     return (
         <div>
-            <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+          <form className={styles.form} >
+        <h3 >Signin</h3>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4R84-rle6PTFGphEMfdaDuo4mM-M1pZZPPg&usqp=CAU"
+          className={styles.img}
+        />
+        
+
+        <div className={styles.btnn}>
+          <label >nationalId</label>
+          <input
+            type="text"
+            placeholder="Enter your ID`"
+            
+            
+          />
         </div>
+
+        <div className={styles.btnn}>
+          <label >Password</label>
+          <input
+            type="password"
+            placeholder="Enter password" />
+        </div>
+
+        <button 
+          type="submit"
+          className={styles.btn}>
+          
+            
+          Signin
+        </button>
+      </form>
+    </div>    
+              
     )
 }
